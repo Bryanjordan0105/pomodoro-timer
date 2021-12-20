7de9b1e26b5df91f70807fb2110b96f7bdd66744
Bryan DelMonte <bryanjordan0105@gmail.com>
2021-12-20 00:24:36 -0500

    added animation to the circle

--- app.js
+++ app.js
@@ -33,5 +33,16 @@ const app = () => {
       play.src = "./svg/play.svg";
     }
   };
+
+  //animating the circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+  };
 };
 app();
