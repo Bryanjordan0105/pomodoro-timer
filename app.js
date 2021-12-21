256a2102b7925e8fe21423067e5c82a8a2f691c8
Bryan DelMonte <bryanjordan0105@gmail.com>
2021-12-21 14:03:58 -0500

    added cursor pointer to play button, rotated timer, enabled different sounds to be chosen, animated timer text.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = () => {
   const sounds = document.querySelectorAll(".sound-picker button");
   // time display
   const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
   // outline length
   const outlineLength = outline.getTotalLength();
   // duration
@@ -16,11 +17,29 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  //pick different sounds
+  sounds.forEach((sound) => {
+    sound.addEventListener("click", function () {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
   //play sound
   play.addEventListener("click", () => {
     checkPlaying(song);
   });
 
+  //select sound
+  timeSelect.forEach((option) => {
+    option.addEventListener("click", function () {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
+  });
   // create a function that stops and plays the sounds
   const checkPlaying = (song) => {
     if (song.paused) {
@@ -43,6 +62,15 @@ const app = () => {
 
     let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
     outline.style.strokeDashoffset = progress;
+    //animating the text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
   };
 };
 app();
